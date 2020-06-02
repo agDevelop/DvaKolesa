9557bb368ccde5c203b455078c3c9ae9a3bc2cdb
Andrew Goncharov <andrewnikolaevich.2014@gmail.com>
2020-06-03 01:31:35 +0300

    -
    -

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,10 +1,3 @@
 export const FireBaseConfig = {
-  apiKey: "AIzaSyAxkWSvObTggP2gaSNg5Pkhz2ScRUWIK3Y",
-  authDomain: "dvakolesa-21a34.firebaseapp.com",
-  databaseURL: "https://dvakolesa-21a34.firebaseio.com",
-  projectId: "dvakolesa-21a34",
-  storageBucket: "dvakolesa-21a34.appspot.com",
-  messagingSenderId: "946670084158",
-  appId: "1:946670084158:web:f6d26ce4528b2eedac316f",
-  measurementId: "G-R8708HC06Z"
+
 };
